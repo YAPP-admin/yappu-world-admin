441967b02dcc819c25dc641e22491cce66a940b9
hyeonjeong <cowguswjd@gmail.com>
2025-03-24 22:56:21 +0900

    feat:가입코드 조회 및 수정 api 추가:YPW-93

---
+++ src/queries/auth/useMemberCodeMutation.ts
@@ -0,0 +1,26 @@
+import { useMutation } from '@tanstack/react-query';
+import { patchMemberCode } from 'apis/auth/AuthApis';
+import { MemberCodeReq } from 'apis/auth/types';
+import { ApiResponse, ErrorResponse } from 'apis/common/types';
+import { AxiosError, AxiosResponse } from 'axios';
+
+export const useMemberCodeMutation = () => {
+  return useMutation<
+    AxiosResponse<ApiResponse<null>>,
+    AxiosError<ErrorResponse>,
+    MemberCodeReq
+  >({
+    mutationFn: (data) => patchMemberCode(data),
+    onSuccess: (res) => {
+      console.log('res :', res);
+    },
+    onError: (err) => {
+      if (err.response) {
+        const errorData = err.response.data;
+        console.error('서버 에러 메세지', errorData.message);
+      } else {
+        console.error('error :', err.message);
+      }
+    },
+  });
+};

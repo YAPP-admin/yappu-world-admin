ef2f2083f6ca81064f2494fbdc9cfc51c245e261
hyeonjeong <cowguswjd@gmail.com>
2025-03-25 23:36:58 +0900

    chore: eslint config 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
-import js from '@eslint/js';
+import eslint from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules', 'vite.config.ts'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -14,6 +15,14 @@ export default tseslint.config(
     },
     plugins: {
       import: importPlugin,
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+          alwaysTryTypes: true,
+        },
+      },
     },
     rules: {
       'import/order': [
@@ -34,14 +43,6 @@ export default tseslint.config(
       ],
       'import/newline-after-import': 'error',
       'import/no-unresolved': 'error',
-      settings: {
-        'import/resolver': {
-          typescript: {
-            project: './tsconfig.json',
-            alwaysTryTypes: true,
-          },
-        },
-      },
     },
   },
 );

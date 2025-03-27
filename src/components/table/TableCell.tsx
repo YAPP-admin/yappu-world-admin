58c8cc8b80ca8b8fe3bf4613b879fa46ad3a57be
hyeonjeong <cowguswjd@gmail.com>
2025-03-27 17:07:53 +0900

    feat:TableCell 정렬 prop추가

--- src/components/table/TableCell.tsx
+++ src/components/table/TableCell.tsx
@@ -1,19 +1,38 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 
+type AlignItems = 'flex-start' | 'flex-end' | 'center';
+type JustifyContent =
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'space-between'
+  | 'space-around';
+
 interface Props {
   as?: 'th' | 'td';
   scope?: 'row' | 'col';
   align?: 'left' | 'center' | 'right';
+  alignItems?: AlignItems;
+  justifyContent?: JustifyContent;
   children?: React.ReactNode;
 }
 
 const TableCell: FC<Props> = (props) => {
-  const { children, as = 'td', scope, align = 'center' } = props;
+  const {
+    children,
+    as = 'td',
+    scope,
+    align = 'center',
+    alignItems = 'flex-start',
+    justifyContent = 'flex-start',
+  } = props;
 
   return (
     <StyledTableCell align={align} as={as}>
-      <Wrapper>{children}</Wrapper>
+      <Wrapper $alignItems={alignItems} $justifyContent={justifyContent}>
+        {children}
+      </Wrapper>
     </StyledTableCell>
   );
 };
@@ -24,8 +43,11 @@ const StyledTableCell = styled.td<{ align: string }>`
   text-align: ${({ align }) => align};
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{
+  $alignItems: AlignItems;
+  $justifyContent: JustifyContent;
+}>`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  align-items: ${({ $alignItems }) => $alignItems};
+  justify-content: ${({ $justifyContent }) => $justifyContent};
 `;

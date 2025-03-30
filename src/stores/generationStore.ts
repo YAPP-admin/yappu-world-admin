f759355202e4a1f16c74128dc606134cb47a568d
hyeonjeong <cowguswjd@gmail.com>
2025-03-30 20:52:55 +0900

    feat: 기수 목록 페이지 생성

---
+++ src/stores/generationStore.ts
@@ -0,0 +1,26 @@
+import { GenerationListRes } from 'apis/operation/types';
+import { create } from 'zustand';
+
+interface GenerationState {
+  selectedGeneration: GenerationListRes | null;
+  setSelectedGeneration: (value: GenerationListRes | null) => void;
+  isMenuOpen: boolean;
+  handleMenuOpen: (value: boolean) => void;
+  isAddPopupOpen: boolean;
+  handleAddPopupOpen: (value: boolean) => void;
+  isAddCompletePopupOpen: boolean;
+  handleAddCompletePopupOpen: (value: boolean) => void;
+}
+
+export const useGenerationStore = create<GenerationState>((set) => ({
+  selectedGeneration: null,
+  setSelectedGeneration: (value: GenerationListRes | null) =>
+    set({ selectedGeneration: value }),
+  isMenuOpen: false,
+  handleMenuOpen: (value: boolean) => set({ isMenuOpen: value }),
+  isAddPopupOpen: false,
+  handleAddPopupOpen: (value: boolean) => set({ isAddPopupOpen: value }),
+  isAddCompletePopupOpen: false,
+  handleAddCompletePopupOpen: (value: boolean) =>
+    set({ isAddCompletePopupOpen: value }),
+}));

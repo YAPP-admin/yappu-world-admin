8162889a7fd527b3313410c732f0e5278a63f260
hyeonjeong <cowguswjd@gmail.com>
2025-04-02 21:55:56 +0900

    feat:칩 타입 유틸 함수 분리

---
+++ src/utils/getChipColor.ts
@@ -0,0 +1,16 @@
+import { ChipColor, ChipStyle } from '@compnents/commons/Chip';
+
+export const getChipColor = (
+  type: string,
+): { color: ChipColor; variant: ChipStyle } => {
+  switch (type) {
+    case '대기':
+      return { color: 'neutral', variant: 'weak' };
+    case '거절':
+      return { color: 'primary', variant: 'fill' };
+    case '승인':
+      return { color: 'secondary', variant: 'fill' };
+    default:
+      return { color: 'neutral', variant: 'weak' };
+  }
+};

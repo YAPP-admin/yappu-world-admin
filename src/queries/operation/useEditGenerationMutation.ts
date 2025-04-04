41302217150c7543acdc83d9aec182c1b1a00cc1
hyeonjeong <cowguswjd@gmail.com>
2025-04-05 03:15:04 +0900

    fix:활동상태 변경 성공 시 목록 재조회

--- src/queries/operation/useEditGenerationMutation.ts
+++ src/queries/operation/useEditGenerationMutation.ts
@@ -1,19 +1,22 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse } from 'axios';
 
+import { useGenerationStore } from '@stores/generationStore';
 import { ApiResponse, ErrorResponse } from 'apis/common/types';
 import { patchGenerationActive } from 'apis/operation/OperationApis';
 import { EditGenerationReq, EditGenerationRes } from 'apis/operation/types';
 
 export const useEditGenerationMutation = () => {
+  const page = useGenerationStore((state) => state.page);
+  const queryClient = useQueryClient();
   return useMutation<
     AxiosResponse<ApiResponse<EditGenerationRes>>,
     AxiosError<ErrorResponse>,
     EditGenerationReq
   >({
     mutationFn: (data) => patchGenerationActive(data),
-    onSuccess: (res) => {
-      console.log('res :', res);
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['generation-list', page] });
     },
     onError: (err) => {
       if (err.response) {

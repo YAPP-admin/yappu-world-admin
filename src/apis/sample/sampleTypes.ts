d91e05e4319ffdbd0c17a2a4ebcf141e7b7176c1
hyeonjeong <cowguswjd@gmail.com>
2025-04-05 01:44:45 +0900

    remove:샘플 코드 제거

--- src/apis/sample/sampleTypes.ts
+++
@@ -1,3 +0,0 @@
-export interface GetSampleDataRes {
-  data: string;
-}
--- src/queries/sample/useSampleDataQuery.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-
-import { getSampleData } from '../../apis/sample/sampleAPI.ts';
-
-const useSampleDataQuery = () =>
-  useQuery({ queryKey: ['sample'], queryFn: () => getSampleData() });
-
-export default useSampleDataQuery;

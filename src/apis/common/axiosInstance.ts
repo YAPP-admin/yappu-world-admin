1047f21e15be90e2c07d78dff2f1065a2e71660f
hyeonjeong <cowguswjd@gmail.com>
2025-05-05 00:25:17 +0900

    fix:토큰 재발급 요청 롤백

--- src/apis/common/axiosInstance.ts
+++ src/apis/common/axiosInstance.ts
@@ -3,7 +3,6 @@ import { toast } from 'react-toastify';
 
 import { commonToastOption } from '@constants/toastOption';
 import { useAuthStore } from '@stores/authStore';
-import { postReissueToken } from 'apis/auth/AuthApis';
 import { showErrorToast } from 'types/showErrorToast';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -32,7 +31,7 @@ axiosInstance.interceptors.response.use(
     if (!error.response) {
       return Promise.reject(error);
     }
-    const { response, config } = error;
+    const { response } = error;
 
     if (
       [500, 400].includes(response.status) &&
@@ -44,33 +43,12 @@ axiosInstance.interceptors.response.use(
       response.status === 401 &&
       ['TKN_0001', 'TKN_0002'].includes(response.data.errorCode)
     ) {
-      const originalRequest = config;
-      try {
-        const { accessToken, refreshToken, setToken } = useAuthStore.getState();
-        if (!accessToken || !refreshToken)
-          throw new Error('token is not exist');
-
-        const { data } = await postReissueToken({ accessToken, refreshToken });
-
-        const newAccessToken = data.data.accessToken;
-        const newRefreshToken = data.data.refreshToken;
-        setToken({
-          accessToken: newAccessToken,
-          refreshToken: newRefreshToken,
-        });
-
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-
-        return axiosInstance(originalRequest);
-      } catch (e) {
-        await useAuthStore.persist.clearStorage();
-        useAuthStore.getState().resetUser();
-        toast.error(
-          `${response.data.message}\n다시 로그인 해주세요.`,
-          commonToastOption,
-        );
-        return Promise.reject(e);
-      }
+      await useAuthStore.persist.clearStorage();
+      useAuthStore.getState().resetUser();
+      toast.error(
+        `${response.data.message}\n다시 로그인 해주세요.`,
+        commonToastOption,
+      );
     }
 
     return Promise.reject(error);

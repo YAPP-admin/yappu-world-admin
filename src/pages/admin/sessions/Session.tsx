7d1f9f0ebb82d5cd15919a65d064b882f0abf843
hyeonjeong <cowguswjd@gmail.com>
2025-05-06 20:51:38 +0900

    fix:세션 수정 이후 목록 아닌 상세 보여지도록

--- src/pages/admin/sessions/Session.tsx
+++ src/pages/admin/sessions/Session.tsx
@@ -5,20 +5,24 @@ import styled from 'styled-components';
 
 import ArrowLeft from '@assets/ArrowLeft';
 import IconButton from '@compnents/Button/IconButton';
+import CompletePopup from '@compnents/popup/CompletePopup';
 import { useSessionDetailQuery } from '@queries/session/useSessionDetailQuery';
+import { useSessionStore } from '@stores/sessionStore';
 import { ErrorResponse } from 'apis/common/types';
 import { showErrorToast } from 'types/showErrorToast';
 
 import SessionDetail from './SessionDetail';
 import SessionEdit from './SessionEdit';
 
-
 const Session: FC = () => {
   const params = useParams();
   const navigate = useNavigate();
   const { data, isError, error } = useSessionDetailQuery(params?.id ?? '');
   const [isEdit, setIsEdit] = useState(false);
-
+  const setEditCompletePopup = useSessionStore(
+    (state) => state.setEditCompletePopup,
+  );
+  const editCompletePopup = useSessionStore((state) => state.editCompletePopup);
   useEffect(() => {
     if (isError) {
       navigate('/admin/sessions');
@@ -46,6 +50,13 @@ const Session: FC = () => {
         <SessionEdit data={data} handleEdit={() => setIsEdit(false)} />
       ) : (
         <SessionDetail data={data} handleEdit={() => setIsEdit(true)} />
+      )}
+      {editCompletePopup && (
+        <CompletePopup
+          comment="세션이 정상적으로 수정 되었습니다."
+          title="세션 수정 완료"
+          onClose={() => setEditCompletePopup(false)}
+        />
       )}
     </Container>
   );

020d7a626a9eba1ffe7db7f64a7fe8cdd7a29ac9
hyeonjeong <cowguswjd@gmail.com>
2025-05-27 20:40:40 +0900

    style:TableCell widthType 으로 변경

--- src/features/attendance/AttendanceHeader.tsx
+++ src/features/attendance/AttendanceHeader.tsx
@@ -15,52 +15,52 @@ const AttendanceHeader: FC<Props> = ({ sessions }) => {
   return (
     <TableHead>
       <TableRow>
-        <TableCell as="th" className="sticky-col-1" />
-        <TableCell as="th" className="sticky-col-2" />
+        <TableCell as="th" className="sticky-col-1" widthType="fixed" />
+        <TableCell as="th" className="sticky-col-2" widthType="fixed" />
         {sessions?.map((el) => (
-          <TableCell key={el.sessionId} max as="th">
+          <TableCell key={el.sessionId} as="th" widthType="max">
             <Typography variant="body1Normal">
               {dayjs(el.startDate).format('YYYY.MM.DD')}
             </Typography>
           </TableCell>
         ))}
-        <TableCell max as="th" />
-        <TableCell max as="th" />
-        <TableCell max as="th" />
-        <TableCell max as="th" />
-        <TableCell max as="th" />
-        <TableCell max as="th" />
+        <TableCell as="th" widthType="max" />
+        <TableCell as="th" widthType="max" />
+        <TableCell as="th" widthType="max" />
+        <TableCell as="th" widthType="max" />
+        <TableCell as="th" widthType="max" />
+        <TableCell as="th" widthType="max" />
       </TableRow>
       <TableRow>
-        <TableCell as="th" className="sticky-col-1">
+        <TableCell as="th" className="sticky-col-1" widthType="fixed">
           <Typography variant="body1Normal">이름</Typography>
         </TableCell>
-        <TableCell as="th" className="sticky-col-2">
+        <TableCell as="th" className="sticky-col-2" widthType="fixed">
           <Typography variant="body1Normal">총점</Typography>
         </TableCell>
         {sessions?.map((el) => (
-          <TableCell key={el.sessionId} max as="th">
+          <TableCell key={el.sessionId} as="th" widthType="max">
             <Typography ellipsis variant="body1Normal">
               {el.name}
             </Typography>
           </TableCell>
         ))}
-        <TableCell max as="th">
+        <TableCell as="th" widthType="max">
           <Typography variant="body1Normal">지각</Typography>
         </TableCell>
-        <TableCell max as="th">
+        <TableCell as="th" widthType="max">
           <Typography variant="body1Normal">조퇴</Typography>
         </TableCell>
-        <TableCell max as="th">
+        <TableCell as="th" widthType="max">
           <Typography variant="body1Normal">지각면제권</Typography>
         </TableCell>
-        <TableCell max as="th">
+        <TableCell as="th" widthType="max">
           <Typography variant="body1Normal">결석</Typography>
         </TableCell>
-        <TableCell max as="th">
+        <TableCell as="th" widthType="max">
           <Typography variant="body1Normal">감점</Typography>
         </TableCell>
-        <TableCell max as="th">
+        <TableCell as="th" widthType="max">
           <Typography variant="body1Normal">가점</Typography>
         </TableCell>
       </TableRow>

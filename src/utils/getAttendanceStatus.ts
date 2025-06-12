39dd79cdbe3c70f9d4ec3c7a3ac3ea96dd015357
hyeonjeong <cowguswjd@gmail.com>
2025-06-12 22:59:49 +0900

    feat:출석 상세에서 미출석/미참여자 추가

--- src/utils/getAttendanceStatus.ts
+++ src/utils/getAttendanceStatus.ts
@@ -1,4 +1,8 @@
-export const getAttendanceStatus = (status: string) => {
+import { AttendanceStatusValueType } from 'apis/attendance/types';
+
+export const getAttendanceStatus = (
+  status: string,
+): AttendanceStatusValueType => {
   switch (status) {
     case 'ON_TIME':
     case '출석':
@@ -15,6 +19,27 @@ export const getAttendanceStatus = (status: string) => {
     case 'EXCUSED_ABSENCE':
     case '공결':
       return 'EXCUSED_ABSENCE';
+    case 'PENDING':
+    case '미출석':
+      return 'PENDING';
+    default:
+      return 'NULL';
+  }
+};
+
+export const substitutionAttendee = (status: string | null) => {
+  if (!status) return '미참여';
+  switch (status) {
+    case 'ON_TIME':
+      return '출석';
+    case 'LATE':
+      return '지각';
+    case 'ABSENT':
+      return '결석';
+    case 'EARLY_CHECK_OUT':
+      return '조퇴';
+    case 'EXCUSED_ABSENCE':
+      return '공결';
     default:
       return '';
   }

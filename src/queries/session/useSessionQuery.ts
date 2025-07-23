c3391808fadbad6268fe82a2446603dcbd52e56d
hyeonjeong <cowguswjd@gmail.com>
2025-07-23 19:42:26 +0900

    feat:세션조회 쿼리 기수 파라미터 추가

--- src/queries/session/useSessionQuery.ts
+++ src/queries/session/useSessionQuery.ts
@@ -2,12 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getSession } from 'apis/session/SessionApis';
 
-export const useSessionQuery = (page: number) => {
+export const useSessionQuery = (
+  page: number,
+  enabled = true,
+  generation?: number,
+) => {
   return useQuery({
     queryKey: ['session-list', page],
-    queryFn: () => getSession({ page, size: 10 }),
+    queryFn: () => getSession({ page, size: 10, generation }),
     select: (data) => {
       return data.data.data;
     },
+    enabled,
   });
 };
